f83080bbf7778fb4e0a9080ff2897de0beb04e6c
eni1221 <yin8213@gmail.com>
2023-10-25 11:38:02 +0900

    NoCommit_6_mistake commit

---
+++ Git_ignore_test/src/main/java/nocommit_some/NoCommit_6_mistake.java
@@ -0,0 +1,14 @@
+package nocommit_some;
+
+public class NoCommit_6_mistake {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 
+		
+		System.out.println("NoCommit_6_mistake");
+
+	}
+
+}

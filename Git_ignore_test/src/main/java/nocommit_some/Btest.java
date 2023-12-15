5a2603df59142c27af2cd9cf69b92f004c3d539e
bicaca <mink0609@naver.com>
2023-12-15 12:39:38 +0900

    커밋

---
+++ Git_ignore_test/src/main/java/nocommit_some/Btest.java
@@ -0,0 +1,11 @@
+package nocommit_some;
+
+public class Btest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("연습용");
+	}
+
+}
---
+++ Git_ignore_test/src/main/java/nocommit_some/Commit_4.java
@@ -0,0 +1,12 @@
+package nocommit_some;
+
+public class Commit_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("commit 4");
+
+	}
+
+}
